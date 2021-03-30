481b45a7c6f2b2741e5a81afb5b980cabdde4545
KennethLloyd <verastiguelloyd@gmail.com>
2021-03-30 16:43:38 +0800

    Add logout functionality

--- client/src/Navigator.js
+++ client/src/Navigator.js
@@ -1,9 +1,11 @@
 import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useQuery } from '@apollo/client';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import { token } from './graphql/reactivities/authVariable';
 import { GET_TOKEN } from './graphql/queries/Reactivity';
@@ -30,6 +32,11 @@ const Navigator = () => {
     readToken();
   }, []);
 
+  const handleLogOut = async () => {
+    await AsyncStorage.removeItem('token');
+    token(null);
+  };
+
   const options = {
     headerStyle: {
       backgroundColor: '#23AAAA',
@@ -37,22 +44,40 @@ const Navigator = () => {
     headerTintColor: '#fff',
   };
 
+  const logOutOption = {
+    headerRight: () => (
+      <MaterialIcons
+        name="logout"
+        style={styles.logOut}
+        size={24}
+        color="white"
+        onPress={handleLogOut}
+      />
+    ),
+  };
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={options}>
         {data && data.token ? (
           <>
-            <Stack.Screen name="Home" component={Home} options={options} />
+            <Stack.Screen name="Home" component={Home} options={logOutOption} />
           </>
         ) : (
           <>
-            <Stack.Screen name="Login" component={Login} options={options} />
-            <Stack.Screen name="Sign Up" component={Signup} options={options} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Sign Up" component={Signup} />
           </>
         )}
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  logOut: {
+    marginRight: 20,
+  },
+});
 
 export default Navigator;
